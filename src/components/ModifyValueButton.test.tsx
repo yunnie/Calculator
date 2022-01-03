798bd43f7dbd1293f41307542073ae64c9f01838
yunnie <steve.s.yun@gmail.com>
2022-01-03 13:37:43 -0800

    Refactored tests

---
+++ src/components/ModifyValueButton.test.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+import {render, fireEvent, screen} from "@testing-library/react"
+import Calculator from "./Calculator"
+
+describe('Modify Value Buttons', () => {
+
+  it('Plus minus toggles number from positive to neg', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId('6'))
+    expect(screen.getByTestId("display")).toHaveTextContent("6")
+    fireEvent.click(screen.getByTestId('+/-'))
+    expect(screen.getByTestId("display")).toHaveTextContent("-6")
+  })
+
+  it('Value remains negative appending to -0', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId('+/-'))
+    expect(screen.getByTestId("display")).toHaveTextContent("-0")
+    fireEvent.click(screen.getByTestId('6'))
+    expect(screen.getByTestId("display")).toHaveTextContent("-6")
+  })
+
+  it('Adding a negative number is the same as subtraction', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId("7"))
+    fireEvent.click(screen.getByTestId("+/-"))
+    fireEvent.click(screen.getByTestId("+"))
+    fireEvent.click(screen.getByTestId("6"))
+    fireEvent.click(screen.getByTestId("="))
+    expect(screen.getByTestId("display")).toHaveTextContent("-1")
+  })
+
+  it('Can convert a number into a percentage', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId("7"))
+    fireEvent.click(screen.getByTestId("6"))
+    expect(screen.getByTestId("display")).toHaveTextContent("76")
+    fireEvent.click(screen.getByTestId("6"))
+    fireEvent.click(screen.getByTestId("%"))
+    expect(screen.getByTestId("display")).toHaveTextContent("7.66")
+  })
+
+  it('Can convert to twice percentage', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId("7"))
+    fireEvent.click(screen.getByTestId("%"))
+    fireEvent.click(screen.getByTestId("%"))
+    expect(screen.getByTestId("display")).toHaveTextContent("0.0007")
+  })
+
+  it('Can add a decimal point', () => {
+    render(<Calculator />) 
+    fireEvent.click(screen.getByTestId("7"))
+    fireEvent.click(screen.getByTestId("."))
+    fireEvent.click(screen.getByTestId("6"))
+    expect(screen.getByTestId("display")).toHaveTextContent("7.6")
+    fireEvent.click(screen.getByTestId("."))
+    fireEvent.click(screen.getByTestId("6"))
+    expect(screen.getByTestId("display")).toHaveTextContent("7.66")
+    fireEvent.click(screen.getByTestId("."))
+  })
+})
---
+++ src/components/OppsButton.test.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+import {render, fireEvent, screen} from "@testing-library/react"
+import Calculator from "./Calculator"
+
+describe('Operation Buttons', () => {
+
+  it('Addition function works', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId('6'))
+    expect(screen.getByTestId("display")).toHaveTextContent("6")
+    fireEvent.click(screen.getByTestId("+"))
+    fireEvent.click(screen.getByTestId('8'))
+    expect(screen.getByTestId("display")).toHaveTextContent("8")
+    fireEvent.click(screen.getByTestId("="))
+    expect(screen.getByTestId("display")).toHaveTextContent("14")
+  })
+
+  it('Subtration function works', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId('4'))
+    expect(screen.getByTestId("display")).toHaveTextContent("4")
+    fireEvent.click(screen.getByTestId("-"))
+    fireEvent.click(screen.getByTestId("9"))
+    expect(screen.getByTestId("display")).toHaveTextContent("9")
+    fireEvent.click(screen.getByTestId("="))
+    expect(screen.getByTestId("display")).toHaveTextContent("-5")
+  })
+
+  it('Multiplication function works', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId('4'))
+    expect(screen.getByTestId("display")).toHaveTextContent("4")
+    fireEvent.click(screen.getByTestId("-"))
+    fireEvent.click(screen.getByTestId("3"))
+    expect(screen.getByTestId("display")).toHaveTextContent("3")
+    fireEvent.click(screen.getByTestId("="))
+    expect(screen.getByTestId("display")).toHaveTextContent("1")
+  })
+
+  it('Multiplication function works', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId('6'))
+    expect(screen.getByTestId("display")).toHaveTextContent("6")
+    fireEvent.click(screen.getByTestId("/"))
+    fireEvent.click(screen.getByTestId("3"))
+    expect(screen.getByTestId("display")).toHaveTextContent("3")
+    fireEvent.click(screen.getByTestId("="))
+    expect(screen.getByTestId("display")).toHaveTextContent("2")
+  })
+
+  it('Can overwrite operation works', () => {
+    render(<Calculator />)
+    fireEvent.click(screen.getByTestId('6'))
+    expect(screen.getByTestId("display")).toHaveTextContent("6")
+    fireEvent.click(screen.getByTestId("+"))
+    fireEvent.click(screen.getByTestId("/"))
+    fireEvent.click(screen.getByTestId("3"))
+    expect(screen.getByTestId("display")).toHaveTextContent("3")
+    fireEvent.click(screen.getByTestId("="))
+    expect(screen.getByTestId("display")).toHaveTextContent("2")
+  })
+})
